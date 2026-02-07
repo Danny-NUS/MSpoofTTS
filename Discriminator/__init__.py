c089d389376aaea39a1935eb00f92526fdc0f55c
vmd <vuminhduc.vmd.vn@gmail.com>
2026-02-07 21:18:15 +0800

    Update PyramidTokenDiscriminator

--- Discriminator/__init__.py
+++ Discriminator/__init__.py
@@ -1,3 +1,4 @@
 from .segment import SegmentTokenDiscriminator
 from .utterance import TokenDiscriminator
 from .suffix import SuffixTokenDiscriminator
+from .pyramid import PyramidTokenDiscriminator
